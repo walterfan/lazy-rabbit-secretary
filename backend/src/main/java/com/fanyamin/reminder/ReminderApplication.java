c7f2f9eebdf6901e5cfc3864c521bafd9d3323c1
Walter Fan <walter.fan@gmail.com>
2024-12-18 23:44:01 +0800

    add docker file and compose file

--- backend/src/main/java/com/fanyamin/reminder/ReminderApplication.java
+++ backend/src/main/java/com/fanyamin/reminder/ReminderApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookManagementApplication {
+public class ReminderApplication {
     public static void main(String[] args) {
-        SpringApplication.run(BookManagementApplication.class, args);
+        SpringApplication.run(ReminderApplication.class, args);
     }
 }
